f0dde01e332c5ee421d42ee5269a43b9bd503dd2
Brandon Trump <brandontrump101@gmail.com>
2023-04-01 01:10:47 +1300

    fetch news & sort by articles with images

---
+++ lib/fetchNews.ts
@@ -0,0 +1,74 @@
+import { gql } from "graphql-request";
+import sortNewsByImage from "./sortNewsByImage";
+
+const fetchNews = async (
+  category?: Category | string,
+  keywords?: string,
+  isDynamic?: boolean
+) => {
+  const query = gql`
+    query MyQuery(
+      $access_key: String!
+      $categories: String!
+      $keywords: String
+    ) {
+      myQuery(
+        access_key: $access_key
+        categories: $categories
+        countries: "gb"
+        sort: "published_desc"
+        keywords: $keywords
+      ) {
+        data {
+          author
+          category
+          image
+          description
+          country
+          language
+          published_at
+          source
+          title
+          url
+        }
+        pagination {
+          count
+          limit
+          offset
+          total
+        }
+      }
+    }
+  `;
+
+  const res = await fetch(
+    "https://balatan.stepzen.net/api/intended-newt/__graphql",
+    {
+      method: "POST",
+      cache: isDynamic ? "no-cache" : "default",
+      next: isDynamic ? { revalidate: 0 } : { revalidate: 20 },
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `ApiKey ${process.env.STEPZEN_API_KEY}`,
+      },
+      body: JSON.stringify({
+        query,
+        variables: {
+          access_key: process.env.MEDIASTACK_API_KEY,
+          categories: category,
+          keywords: keywords,
+        },
+      }),
+    }
+  );
+
+  console.log("LOADING NEW DATA FROM API for category >>>", category, keywords);
+
+  const newsResponse = await res.json();
+
+  const news = sortNewsByImage(newsResponse.data.myQuery);
+
+  return news;
+};
+
+export default fetchNews;
